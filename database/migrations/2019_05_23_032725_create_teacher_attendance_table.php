15a09015dc19aebe462a5099381e3ade412103b0
Fajar Jr <fajarjunyor13@gmail.com>
2019-05-23 11:12:46 +0700

    make migration

--- database/migrations/2019_05_23_032725_create_teacher_attendance_table.php
+++ database/migrations/2019_05_23_032725_create_teacher_attendance_table.php
@@ -15,6 +15,13 @@ class CreateTeacherAttendanceTable extends Migration
     {
         Schema::create('teacher_attendance', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('id_teacher')->unsigned();
+            $table->string('course', 20);
+            $table->date('date');
+            $table->time('time_start');
+            $table->time('time_finish');
+            $table->string('status', 20);
+            $table->boolean('attendance_block')->nullable()->default(false);
             $table->timestamps();
         });
     }
